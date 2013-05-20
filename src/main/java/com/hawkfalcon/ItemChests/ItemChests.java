b58db4abbab4b8895c51507f82e1a8f94b8b523a
lol768 <support@technostripe.com>
2013-05-20 18:33:52 +0100

    Add code

---
+++ src/main/java/com/hawkfalcon/ItemChests/ItemChests.java
@@ -0,0 +1,46 @@
+package com.hawkfalcon.ItemChests;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitTask;
+import org.bukkit.event.Listener;
+
+public class ItemChests extends JavaPlugin {
+    public boolean infinite = true;
+    public Integer limit = 1;
+    public CommandExecutor Commands = new Commands(this);
+    public Listener InventoryListener = new InventoryListener(this);
+    public HashMap<String, Integer> playerLimit = new HashMap<String, Integer>();
+
+    public void onEnable() {
+        final File f = new File(getDataFolder(), "config.yml");
+        if (!f.exists()) {
+            saveDefaultConfig();
+        }
+        try {
+            MetricsLite metrics = new MetricsLite(this);
+            metrics.start();
+        } catch (IOException e) {
+            System.out.println("Error Submitting stats!");
+        }
+        getServer().getPluginManager().registerEvents(InventoryListener, this);
+        getCommand("ic").setExecutor(Commands);
+        infinite = getConfig().getBoolean("infinite");
+        limit = getConfig().getInt("limit");
+        startTimer();
+    }
+
+    private void startTimer() {
+        @SuppressWarnings("unused")
+        BukkitTask task = Bukkit.getScheduler().runTaskTimer(this, new Runnable() {
+            public void run() {
+                playerLimit.clear();
+            }
+        }, 0, 24 * 60 * 60 * 20);
+    }
+}
